3579669b1f736a286b2ab16177311857051aae21
nadiashabeer <nadiaadis.tms@gmail.com>
2025-08-27 18:14:08 -0400

    portfolio

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,36 @@
+import "./intro.css";
+import { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+export default function Intro() {
+  const item = useRef();
+
+  useEffect(() => {
+    init(item.current, {
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer.", "Designer.", "Student."],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2 className="intro-animate">Hi There, I'm</h2>
+          <h1 className="intro-animate">Nadia Shabeer</h1>
+          <h3 className="intro-animate">
+            A Freelance <span ref={item}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
+    </div>
+  );
+}
